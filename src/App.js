d30c76a24324250b0224d5963074fa5022535a12
Manuel-Ascencio <manuelascencioprz@gmail.com>
2022-10-04 16:43:45 -0500

    update readme

--- src/App.js
+++ src/App.js
@@ -1,56 +1,9 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import Drum from "./components/Drum";
 import { NameContext } from "./context/NameContext";
 
 function App() {
-  const { soundName } = useContext(NameContext);
-  const [sounds, setSounds] = useState([
-    {
-      name: "boom",
-      sound: require("./sounds/boom.wav"),
-      key: "Q",
-    },
-    {
-      name: "clap",
-      sound: require("./sounds/clap.wav"),
-      key: "W",
-    },
-    {
-      name: "hihat",
-      sound: require("./sounds/hihat.wav"),
-      key: "E",
-    },
-    {
-      name: "kick",
-      sound: require("./sounds/kick.wav"),
-      key: "A",
-    },
-    {
-      name: "openhat",
-      sound: require("./sounds/openhat.wav"),
-      key: "S",
-    },
-    {
-      name: "ride",
-      sound: require("./sounds/ride.wav"),
-      key: "D",
-    },
-    {
-      name: "snare",
-      sound: require("./sounds/snare.wav"),
-      key: "Z",
-    },
-    {
-      name: "tink",
-      sound: require("./sounds/tink.wav"),
-      key: "X",
-    },
-    {
-      name: "tom",
-      sound: require("./sounds/tom.wav"),
-      key: "C",
-    },
-  ]);
+  const { soundName, sounds } = useContext(NameContext);
 
   return (
     <div className="App">
--- src/context/NameContext.js
+++ src/context/NameContext.js
@@ -4,8 +4,58 @@ export const NameContext = createContext();
 
 export const NameProvider = ({ children }) => {
   const [soundName, setSoundName] = useState("");
+  const [sounds, setSounds] = useState([
+    {
+      name: "boom",
+      sound: require("../sounds/boom.wav"),
+      key: "Q",
+    },
+    {
+      name: "clap",
+      sound: require("../sounds/clap.wav"),
+      key: "W",
+    },
+    {
+      name: "hihat",
+      sound: require("../sounds/hihat.wav"),
+      key: "E",
+    },
+    {
+      name: "kick",
+      sound: require("../sounds/kick.wav"),
+      key: "A",
+    },
+    {
+      name: "openhat",
+      sound: require("../sounds/openhat.wav"),
+      key: "S",
+    },
+    {
+      name: "ride",
+      sound: require("../sounds/ride.wav"),
+      key: "D",
+    },
+    {
+      name: "snare",
+      sound: require("../sounds/snare.wav"),
+      key: "Z",
+    },
+    {
+      name: "tink",
+      sound: require("../sounds/tink.wav"),
+      key: "X",
+    },
+    {
+      name: "tom",
+      sound: require("../sounds/tom.wav"),
+      key: "C",
+    },
+  ]);
+
   return (
-    <NameContext.Provider value={{ soundName, setSoundName }}>
+    <NameContext.Provider
+      value={{ soundName, setSoundName, sounds, setSounds }}
+    >
       {children}
     </NameContext.Provider>
   );
